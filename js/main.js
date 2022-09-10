16b14f121346fac4766eb5e712cd6f2d3160676c
Rachael-Myers <mantaloversmomma@gmail.com>
2022-09-10 16:05:23 -0700

    formatting and bug fixes...again

--- js/main.js
+++ js/main.js
@@ -22,31 +22,31 @@ function spin(){
 
 
     let ALGIZ = shuffle([1890, 2250, 2610]);
-    let Camera = shuffle([1850, 2210, 2570]);
-    let Zonk = shuffle([1770, 2130, 2490]);
-    let PS = shuffle([1810, 2170, 2530]);
-    let Headset = shuffle([1750, 2110, 2470]);
-    let Drone = shuffle([1630, 1990, 2350])
-    let ROG = shuffle([1570, 1930, 2290]);
+    let URUZ = shuffle([1850, 2210, 2570]);
+    let LAGUZ = shuffle([1770, 2130, 2490]);
+    let RAIDO = shuffle([1810, 2170, 2530]);
+    let DAGAZ = shuffle([1750, 2110, 2470]);
+    let WUNJO = shuffle([1630, 1990, 2350])
+    let FEHU = shuffle([1570, 1930, 2290]);
 
     let results = shuffle([
         ALGIZ[0],
-        Camera[0],
-        Zonk[0],
-        PS[0],
-        Headset[0],
-        Drone[0],
-        ROG[0],
+        URUZ[0],
+        LAGUZ[0],
+        RAIDO[0],
+        DAGAZ[0],
+        WUNJO[0],
+        FEHU[0],
     ]);
 
 
 if(ALGIZ.includes(results[0])) SelectedItem = "A L G I Z";
-if(Camera.includes(results[0])) SelectedItem = "U R U Z";
-if(Zonk.includes(results[0])) SelectedItem = "L A G U Z";
-if(PS.includes(results[0])) SelectedItem = "R A I D O";
-if(Headset.includes(results[0])) SelectedItem = "D A G A Z";
-if(Drone.includes(results[0])) SelectedItem = "W U N J O";
-if(ROG.includes(results[0])) SelectedItem = "F E H U";
+if(URUZ.includes(results[0])) SelectedItem = "U R U Z";
+if(LAGUZ.includes(results[0])) SelectedItem = "L A G U Z";
+if(RAIDO.includes(results[0])) SelectedItem = "R A I D O";
+if(DAGAZ.includes(results[0])) SelectedItem = "D A G A Z";
+if(WUNJO.includes(results[0])) SelectedItem = "W U N J O";
+if(FEHU.includes(results[0])) SelectedItem = "F E H U";
 
 box.style.setProperty("transition", "all ease 5s");
 box.style.transform = "rotate(" + results[0] + "deg)";
